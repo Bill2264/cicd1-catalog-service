0f62a1897b6411177cfffee492cba567c1f52127
BillyLis-Murphy-STUD <G00465240@atu.ie>
2026-09-16 14:34:26 +0100

    Added Product to Model package

---
+++ src/main/java/ie/atu/cicd1catalogservice/Model/Product.java
@@ -0,0 +1,17 @@
+package ie.atu.cicd1catalogservice.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private long id;
+    private String name;
+    private BigDecimal price;
+}
