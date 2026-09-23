6860a3f5e90ee4d1d244ea8e63da2669f436fc0f
BillyLis-Murphy-STUD <G00465240@atu.ie>
2026-09-23 14:40:17 +0100

    Added ProductRepository for JPA stuff

---
+++ src/main/java/ie/atu/cicd1catalogservice/Repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package ie.atu.cicd1catalogservice.Repository;
+
+import ie.atu.cicd1catalogservice.Model.Product;
+import org.springframework.data.repository.Repository;
+
+public interface ProductRepository extends Repository<Product,Long> {
+}
