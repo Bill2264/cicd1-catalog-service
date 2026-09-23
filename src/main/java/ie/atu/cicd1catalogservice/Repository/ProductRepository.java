486c62bf4e4a4376285da4e01aa0aa5bf61ae2d9
BillyLis-Murphy-STUD <G00465240@atu.ie>
2026-09-23 14:55:33 +0100

    Changed ProductService for JPA

--- src/main/java/ie/atu/cicd1catalogservice/Repository/ProductRepository.java
+++ src/main/java/ie/atu/cicd1catalogservice/Repository/ProductRepository.java
@@ -1,7 +1,8 @@
 package ie.atu.cicd1catalogservice.Repository;
 
 import ie.atu.cicd1catalogservice.Model.Product;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends Repository<Product,Long> {
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
